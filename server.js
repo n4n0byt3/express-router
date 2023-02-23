d45d3159c18574e241300ba980e257d4684c35e6
n4n0byt3 <shivbo1@hotmail.com>
2023-02-23 12:13:03 +0000

    completed exercise 1 and 2

--- server.js
+++ server.js
@@ -42,11 +42,69 @@ let fruits = [
     },
 ]
 
+app.use(express.json())
+
 // Express Routes
+app.get('/users', async (req, res) => {
+    res.send(users)
+});
 
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(users[id]);
+});
 
+app.post('/users', (req, res) => {
+    const newUser = req.body
+    users.push(newUser)
+    res.send(`User added: ${newUser.name}`)
+})
 
+app.put('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedUser = req.body;
+    if (users[id]) {
+      users[id] = updatedUser;
+      res.send('User updated successfully');
+    } else {
+      res.status(404).send('User not found');
+    }
+  });
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params.id
+    users.splice(id, 1)
+    res.send(`User deleted`)
+})
+
+app.get('/fruits', async (req, res) =>{
+    res.send(fruits)
+})
+
+app.get('/fruits/:id', async (req, res) => {
+    const id = req.params.id;
+    res.send(fruits[id]);
+})
+
+app.post('/fruits', (req, res) => {
+    const newFruit = req.body
+    fruits.push(newFruit)
+    res.send(`Fruit added: ${newFruit.name}`)
+})
+
+app.put("/fruits/:index", (req, res) => {
+    const index = req.params.index;
+    const fruit = req.body;
+    fruits[index] = fruit;
+    res.send(`Fruit at index ${index} has been updated`);
+  });
+
+app.delete('/fruits/:id', (req, res) => {
+    const id = req.params.id
+    fruits.splice(id, 1)
+    res.send(`Fruit deleted`)
+})
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
-})
+});
